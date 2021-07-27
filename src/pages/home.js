4f5e0cc3282f21d57c4aa57aaab0b6c710f65e0b
lumat18 <lumat@op.pl>
2021-07-28 00:38:20 +0200

    create feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { FooterContainer } from '../containers/footer';
 import { FaqsContainer } from '../containers/faqs';
 import { JumbotronContainer } from '../containers/jumbotron';
+import { HeaderContainer } from '../containers/header';
+import { Feature, OptForm } from '../components';
 
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>Ready to watch? Enter you email to create or restart your membership</OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
