3bec15db90670b90d76f1e07ffde37ee5204e26f
lumat18 <lumat@op.pl>
2021-08-02 17:02:50 +0200

    fetch data from firestore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, Browse, SignUp, SignIn } from './pages';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 import * as ROUTES from './constants/routes';
 
 export default function App() {
+  const { user } = useAuthListener();
+
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
       <Route exact path={ROUTES.SIGN_UP}>
-        <SignUp />
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
+          <SignUp />
+        </IsUserRedirect>
       </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <SignIn />
+      <Route path={ROUTES.SIGN_IN}>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
+          <SignIn />
+        </IsUserRedirect>
       </Route>
+      <ProtectedRoute user={user} exact path={ROUTES.BROWSE}>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+        <Home />
+      </IsUserRedirect>
     </Router>
   );
 }
