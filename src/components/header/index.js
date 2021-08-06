efdbc97b96afc7d5136671e46882f6e5d67e5acd
lumat18 <lumat@op.pl>
2021-08-06 13:42:34 +0200

    add header in the browse page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Background, Container, Logo, ButtonLink } from './styles/header';
+import { Background, Container, Logo, ButtonLink, Feature, Text, FeatureCallOut } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children;
 }
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
@@ -20,4 +24,12 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
 
 Header.Button = function HeaderButton({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
 };
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,9 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
-import { Loading } from '../components';
+import { Loading, Header } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -17,5 +19,24 @@ export function BrowseContainer({ slides }) {
 
   const loadingProfile = loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />;
 
-  return profile.displayName ? loadingProfile : <SelectProfileContainer user={user} setProfile={setProfile} />;
+  return profile.displayName ? (
+    <>
+      {loadingProfile}
+      <Header src="joker1">
+        <Header.Frame>
+          <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks connetciton as he walks the streets of Gothan
+            City. Arthur wears two masks -- the on he paints for his day job as a clown and the guise he projects in a
+            futile attempt to feel like he's part of the world around him.
+          </Header.Text>
+        </Header.Feature>
+      </Header>
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
